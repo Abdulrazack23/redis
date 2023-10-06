fafe79162f8277cc9ed1366b5c52f05c40dc0771
Abdulrazack23 <abdulzack8@gmail.com>
2023-10-06 11:05:13 +0300

    add fastapi with redis

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from redis import Redis
+import httpx
+import json
+
+app= FastAPI
+
+@app.on_event("startup")
+async def startuo_event():
+    app.state.redis =Redis(host ='localhost', port=6379)
+    app.state.http_client =httpx.AsyncClient()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    app.state.redis.close()
+
+@app.get('entries')
+async def read_item():
+    value =app.state.redis.get('entries')
+    if value is None:
+       response =await app.state.http_client.get('https://api.publicapis.org/enttries')
+       return response.json()
+
